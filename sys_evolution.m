659d6f526b84e59054b3b965186e756a0fa87520
nicsalv <you@example.com>
2019-04-30 16:48:07 +0200

    Aggiunte componenti modello
    Sono state aggiunte le componenti del modello del paper, come richiesto per i conti LQ.

--- sys_evolution.m
+++ sys_evolution.m
@@ -4,6 +4,16 @@
 
 clear;
 
+% Raccolta dei dati dal file Excel
+theta_a = xlsread('dati.xlsx', 'F1:F25');
+theta_c_star = xlsread('dati.xlsx', 'C1:C25');
+
+% Parametri temporali
+T = 24; % Orizzonte
+sampling_time = .1; % Un secondo
+t = 0 : sampling_time : T; % Asse dei tempi
+hours = (0 : length(theta_a)-1)'; % Indice delle ore per le temperature
+
 % Costanti numeriche
 cc = 2.15;
 mc = 2;
@@ -21,15 +31,29 @@ sf = 6;
 k_bar_af = 1.22;
 kaf = k_bar_af * sf;
 
-% Disturbo deterministico
-theta_a = xlsread('dati.xlsx', 'F1:F25');
-hours = (0 : length(theta_a)-1)';
+% Matrici in tempo continuo
+Ac = [-kfc/Cc, kfc/cc; ...
+    kfc/Cf, -(kfc+kaf)/Cf];
+Bc = [0 1/Cf]';
+C = [0 1];
+sys_c = ss(Ac,Bc,C,0);
+
+% Matrici in tempo discreto
+sys_d = c2d(sys_c, sampling_time);
+A = sys_d.A;
+B = sys_d.B;
 
 % Distrubi stocastici
 % Deviazioni standard (radici della Varianza)
 c_std = 1 / Cc;
 f_std = 1 / Cf;
 
-% Parametri temporali
-T = 24; % Orizzonte
-sampling_time = 1 / 3600; % Un secondo
+% Definizione delle quantità come nel paper
+% Le matrici A e B sono già inserite
+z = zeros(2, length(t));
+u = zeros(2, length(t)-1);
+mi = zeros(2, length(t)-1);
+eta = [zeros(1, length(t)); interp1(hours, theta_a, t)];
+z_hat = [interp1(hours, theta_c_star, t); zeros(1, length(t))];
+
+plot(hours,theta_c_star,'o', t,z_hat(1,:),':.');
