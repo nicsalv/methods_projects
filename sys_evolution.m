f1ab56744aace4b4f339cceb9c83d121304c0c9a
nicsalv <you@example.com>
2019-05-06 18:09:24 +0200

    Added discretization
    The system has been discretized by means of Euler's approximation.

--- sys_evolution.m
+++ sys_evolution.m
@@ -10,8 +10,8 @@ theta_c_star = xlsread('dati.xlsx', 'C1:C25');
 
 % Parametri temporali
 T = 24; % Orizzonte
-sampling_time = .1; % Un secondo
-t = 0 : sampling_time : T; % Asse dei tempi
+deltaT = 1 / 3600; % Un secondo
+t = 0 : deltaT : T; % Asse dei tempi
 hours = (0 : length(theta_a)-1)'; % Indice delle ore per le temperature
 
 % Costanti numeriche
@@ -32,28 +32,53 @@ k_bar_af = 1.22;
 kaf = k_bar_af * sf;
 
 % Matrici in tempo continuo
-Ac = [-kfc/Cc, kfc/cc; ...
+A = [-kfc/Cc, kfc/Cc; ...
     kfc/Cf, -(kfc+kaf)/Cf];
-Bc = [0 1/Cf]';
+B = [0 1/Cf]';
+Ba = [0 kaf/Cf]';
 C = [0 1];
-sys_c = ss(Ac,Bc,C,0);
-
-% Matrici in tempo discreto
-sys_d = c2d(sys_c, sampling_time);
-A = sys_d.A;
-B = sys_d.B;
 
 % Distrubi stocastici
-% Deviazioni standard (radici della Varianza)
-c_std = 1 / Cc;
-f_std = 1 / Cf;
+c_std = deltaT / Cc; % Deviazioni standard (radici della Varianza)
+f_std = deltaT / Cf;
+csi_std = .5^.5;
+omega_c = c_std .* randn(1, length(t)-1);
+omega_f = f_std .* randn(1, length(t)-1);
+omega_tilde = [omega_c; omega_f];
+csi_f = csi_std .* randn(1, length(t)-1);
+stats = [mean(omega_f) std(omega_f) var(omega_f)];
 
-% Definizione delle quantità come nel paper
-% Le matrici A e B sono già inserite
-z = zeros(2, length(t));
-u = zeros(2, length(t)-1);
-mi = zeros(2, length(t)-1);
-eta = [zeros(1, length(t)); interp1(hours, theta_a, t)];
-z_hat = [interp1(hours, theta_c_star, t); zeros(1, length(t))];
+% Risposta libera del sistema discreto
+x = zeros(2, length(t));
+q = ones(1, length(t)-1) * 0;
+y = zeros(1, length(t));
+x(:,1) = [293; 291];
 
-plot(hours,theta_c_star,'o', t,z_hat(1,:),':.');
+theta_a = interp1(hours, theta_a, t);
+
+A_tilde = A .* deltaT + eye(size(A));
+B_tilde = deltaT .* B;
+Ba_tilde = deltaT .* Ba;
+
+for k = 1 : length(t) - 1
+    % La k-esima colonna di x corrisponde all'istante k*deltaT
+    x(:,k+1) = A_tilde * x(:,k) + B_tilde * q(:,k) ...
+        + Ba_tilde * theta_a(k) + omega_tilde(:,k);
+    y(k) = C * x(:,k) + csi_f(k);
+end
+y(end) = y(end-1); % Solamente per plottare correttamente
+
+stairs(t, [x' theta_a' y']);
+legend('x1','x2','theta_a','y');
+
+eig(A_tilde)
+ 
+% % Definizione delle quantità come nel paper
+% % Le matrici A e B sono già inserite
+% z = zeros(2, length(t));
+% u = zeros(2, length(t)-1);
+% mi = zeros(2, length(t)-1);
+% eta = [zeros(1, length(t)); interp1(hours, theta_a, t)];
+% z_hat = [interp1(hours, theta_c_star, t); zeros(1, length(t))];
+% 
+% plot(hours,theta_c_star,'o', t,z_hat(1,:),':.');
